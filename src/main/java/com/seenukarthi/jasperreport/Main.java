a676286b2d1a6ea224549f63f0e3afc16a43efaa
Karthikeyan Vaithilingam <seenukarthi@gmail.com>
2014-08-04 11:25:32 +0400

    Restructure

---
+++ src/main/java/com/seenukarthi/jasperreport/Main.java
@@ -0,0 +1,10 @@
+package com.seenukarthi.jasperreport;
+
+/**
+ * Created by s766952 on 4/8/2014.
+ */
+public class Main {
+    public static void main(String a[]){
+        System.out.println("Welcome to JasperReports");
+    }
+}
